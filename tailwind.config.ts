108613b8cb79ffdd9bbe1fa26300d0cf5d8557f4
Ekasuti <emmanuel.k.makau.jr.26@dartmouth.edu>
2025-02-05 21:44:33 -0500

    tailwind css update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,22 +3,22 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-      extend: {
-          colors: {
-              primary: {
-                  DEFAULT: '#01693E',
-                  foreground: 'hsl(var(--primary-foreground))'
-              },
-              'stats-purple': '#F2EEFD',
-              'stats-blue': '#EDF8FA',
-              'stats-orange': '#FEF5EB',
-              'stats-green': '#F3FCF3',
-          }
-      }
-  },
-  plugins: [require("tailwindcss-animate")],
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#01693E',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                'stats-purple': '#F2EEFD',
+                'stats-blue': '#EDF8FA',
+                'stats-orange': '#FEF5EB',
+                'stats-green': '#F3FCF3',
+            }
+        }
+    },
+    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
