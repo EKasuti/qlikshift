72afc7c855f3d5b1adcb00b319a8cb787d15fe32
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2025-03-26 18:14:35 -0400

    Switched back to development

--- backend/src/app/api/students/term/route.ts
+++ backend/src/app/api/students/term/route.ts
@@ -76,6 +76,7 @@ function parseTimeSlotHeader(header: string): { day: string; time: string } {
     return { day, time: `${startTime} - ${endTime}` };
 }
 
+
 // POST function to store Term Students from excel sheet
 export async function POST(request: Request) {
     const origin = request.headers.get('origin');
