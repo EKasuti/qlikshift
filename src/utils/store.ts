3f706af50cdea82d12ebb5c650c68dbd641e264f
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2025-03-27 12:10:38 -0400

    Added user to store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,14 +1,17 @@
 import { AssignmentSlice, AssignmentState } from '@/slice/assignSlice';
 import { DeskSlice, DeskState } from '@/slice/deskSlice';
 import { StudentsSlice, StudentsState } from '@/slice/studentsSlice';
+import { UserSlice, UserState } from '@/slice/userSlice';
 import { StateCreator } from 'zustand';
 
 // Combining all slices into one store
-export type StoreState = StudentsState & DeskState & AssignmentState;
+export type StoreState = StudentsState & DeskState & AssignmentState & UserState;
 
 // 2. Create store by combining slices
 export const createStore: StateCreator<StoreState> = (...a) => ({
+  ...UserSlice(...a),
   ...StudentsSlice(...a),
   ...DeskSlice(...a),
   ...AssignmentSlice(...a),
+
 });
